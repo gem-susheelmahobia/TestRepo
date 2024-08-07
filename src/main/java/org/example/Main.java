f7eef51b5a5de81fc95f11e34bf78899b3094ca3
gem-susheelmahobia <susheel.mahobia@geminisolutions.com>
2024-08-07 20:47:39 +0530

    commited1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("testProjectr1");
         System.out.println("endd");
+        System.out.println("hello");
+
     }
 }
