794eb68164b8f16f9f988dfd03d6ed6572151129
gem-susheelmahobia <susheel.mahobia@geminisolutions.com>
2024-08-07 20:31:42 +0530

    Resolved conflicts after pulling changes and applying stash

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World222222222222222222222");
-        System.out.println("hello");
+        System.out.println("testProjectr1");
     }
 }
